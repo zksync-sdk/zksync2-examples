44c8d14825a63a11f83378fa1d989f668d2caf3d
Danijel Radakovic <danijel@txfusion.io>
2023-10-03 00:27:17 +0200

    fix: add zksync2-js dependency and typescript config

--- js/src/02_transfer.ts
+++ js/src/02_transfer.ts
@@ -8,22 +8,20 @@ const wallet = new Wallet(PRIVATE_KEY, provider, ethProvider);
 
 
 async function main() {
-    const token = "0xCd9BDa1d0FC539043D4C80103bdF4f9cb108931B";
     const receiver = "0x81E9D85b65E9CC8618D85A1110e4b1DF63fA30d9";
 
-    console.log(`Account1 balance before transfer: ${await wallet.getBalance(token)}`);
-    console.log(`Account2 balance before transfer: ${await provider.getBalance(receiver, 'latest', token)}`);
+    console.log(`Account1 balance before transfer: ${await wallet.getBalance()}`);
+    console.log(`Account2 balance before transfer: ${await provider.getBalance(receiver)}`);
 
     const tx = await wallet.transfer({
-        token: token,
         to: receiver,
-        amount: 5,
+        amount: ethers.parseEther("0.01"),
     });
     const receipt =  await tx.wait();
     console.log(`Tx: ${receipt.hash}`);
 
-    console.log(`Account1 balance after transfer: ${await wallet.getBalance(token)}`);
-    console.log(`Account2 balance after transfer: ${await provider.getBalance(receiver, 'latest', token)}`);
+    console.log(`Account1 balance after transfer: ${await wallet.getBalance()}`);
+    console.log(`Account2 balance after transfer: ${await provider.getBalance(receiver)}`);
 }
 
 main().then().catch(error => {
