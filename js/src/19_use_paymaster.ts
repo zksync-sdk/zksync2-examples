0467e1ca15c4da9b2c872d3149dbd4824d68beef
petarTxFusion <petar@txfusion.io>
2024-01-19 19:57:48 +0100

    chore: lint code

--- js/src/19_use_paymaster.ts
+++ js/src/19_use_paymaster.ts
@@ -6,7 +6,6 @@ const ethProvider = ethers.getDefaultProvider("sepolia");
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const wallet = new Wallet(PRIVATE_KEY, provider, ethProvider);
 
-
 const tokenAddress = "0xA70dF8446A6AeA0017D60e97e816e141aa28759b"; // Crown token which can be minted for free
 const paymasterAddress = "0x57F48f0d845E0ed7C9Bf066cEbFF64FbeBE6AFEF"; // Paymaster for Crown token
 const token = new ethers.Interface(
